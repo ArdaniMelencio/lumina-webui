5cda0a8dde46638a162a7e351cf88d17b09ab3ac
ArdaniMelencio <ardanimelencio@gmail.com>
2026-03-24 21:03:18 +0800

    fix: change search query
    - Changed search query of ddgs_test for consistency

--- backend/tests/ddgs_test.py
+++ backend/tests/ddgs_test.py
@@ -3,7 +3,7 @@ from backend.src import ddgs_handler
 def main():
     searcher = ddgs_handler.Search_Handler()
     
-    results = searcher.search("Who is pewdiepie?")    
+    results = searcher.search("Why is the sky blue?")    
     print(results)
 
 
