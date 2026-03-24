234f1b53bbd5dc3ff2161fc2ba42711bb0467d78
ArdaniMelencio <ardanimelencio@gmail.com>
2026-03-24 11:46:10 +0800

    feat(api): create ddgs handler
    - Handles online search queries

---
+++ backend/src/ddgs_handler.py
@@ -0,0 +1,23 @@
+from ddgs import DDGS
+
+class Search_Handler():
+    
+    def __init__(self):
+        self.ddgs = DDGS()
+        self.search_engine = 'duckduckgo'
+        self.max_results = 5
+        self.timeout = 20   # Timeout in seconds
+        
+    def search(self, query: str) -> []:
+        
+        # loop to reattempt when search fails
+        while True:
+            try:
+                return self.ddgs.text(
+                    query = query,
+                    max_results = self.max_results,
+                    backend = self.search_engine,
+                    timeout = self.timeout
+                )
+            except Exception as ERR:
+                print(ERR)
