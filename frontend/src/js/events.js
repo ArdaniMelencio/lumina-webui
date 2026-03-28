84caa51f82c11c28ff4bd00004bbad29cf482a9c
ArdaniMelencio <ardanimelencio@gmail.com>
2026-03-28 10:33:20 +0800

    feat(UI): add form-input resizing

--- frontend/src/js/events.js
+++ frontend/src/js/events.js
@@ -1,3 +1,11 @@
 
 
 function __init_events(e){
+
+    resize_input();
+
+    INPUT_TEXT.addEventListener('input', async function(event) {
+        event.preventDefault();
+
+        resize_input();
+    });
