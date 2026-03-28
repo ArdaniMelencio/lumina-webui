2a36ad46a5ad77c9a62f831d19f024fa5f83275c
ArdaniMelencio <ardanimelencio@gmail.com>
2026-03-28 10:31:41 +0800

    feat(UI): add user message handling
    - Shows user message in display
    - Sends INFO to log if --debug is enabled

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -3,3 +3,45 @@ document.addEventListener('DOMContentLoaded', async function(e) {
 
     __init_events();
 })
+
+
+function handleUserInput(message) {
+
+    if (INPUT_TEXT.value == '') return;
+
+        if (!CHATBOX.classList.contains('hasMessage')) {
+            CHATBOX.classList.add('hasMessage');
+            CHATBOX.textContent = '';
+        }
+
+    
+    message_handler('user', message);
+    resize_input();
+}
+
+function message_handler(sender, message)  {
+        
+    msgDiv = document.createElement('div');
+
+    // handle different messages
+    switch (sender) {
+        case 'user':
+            user(msgDiv, message)
+            break;
+        default: 
+            pywebview.api.log("No valid sender", 30)
+    }
+}
+
+function user(msgDiv, message) {
+
+    pywebview.api.log("Submitted by user: " + message, 10);
+
+    msgDiv.textContent = message;
+    msgDiv.classList.add('message', 'user')
+    
+    CHATBOX.appendChild(msgDiv)
+    INPUT_TEXT.value = '';
+    
+    chatbox.scrollTop = chatbox.scrollHeight;
+}
