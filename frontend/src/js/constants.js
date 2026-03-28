07d2e39d41b422da7d9dfba3ce47026aa2ca85d3
ArdaniMelencio <ardanimelencio@gmail.com>
2026-03-28 10:43:55 +0800

    refactor: move constants
    - added CHATBOX
    - added Settings constants
        - SETTINGS_BTN
        - SETTINGS

--- frontend/src/js/constants.js
+++ frontend/src/js/constants.js
@@ -1,3 +1,9 @@
+// Messaging
+const CHATBOX    = document.getElementById("chatbox");
 const INPUT_FORM = document.getElementById("user-entry");
 const INPUT_TEXT = document.getElementById("form-input");
 const INPUT_BTN  = document.getElementById("form-submit");
+
+// Settings
+const SETTINGS_BTN  = document.getElementById("header-settings-button"); 
+const SETTINGS      = document.getElementById("settings");
