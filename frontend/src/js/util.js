eefc0376e4e68bcbca9ddfd85990a10989716d54
ArdaniMelencio <ardanimelencio@gmail.com>
2026-03-30 08:53:29 +0800

    feat: add setting display switch function

--- frontend/src/js/util.js
+++ frontend/src/js/util.js
@@ -5,7 +5,21 @@ function resize_input(){
     INPUT_TEXT.style.height = `${INPUT_TEXT.scrollHeight + 2}px` ;
 }
 
+
 function scroll_down(){
     // Moves scroll box down to lowest
     CHATBOX.scrollTop = CHATBOX.scrollHeight;
+}
+
+function changeSettingsDisplay(button){
+    const panelName = button.dataset.panel;
+    const templateId = `${panelName}-panel`;
+    const template = document.getElementById(templateId);
+
+    const display =  document.getElementById('settings-display');
+    const panels = display.querySelectorAll('[class="display"]');
+    panels.forEach(panel => {
+        panel.style.display = 'none';
+    })
+    template.style.display = 'block';
 }
