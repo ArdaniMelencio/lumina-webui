09aa0c74068fca1da242b4a47fad9aabf2e7ed50
ArdaniMelencio <ardanimelencio@gmail.com>
2026-03-30 13:11:12 +0800

    feat(UI): add submit animation

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -13,16 +13,26 @@ window.addEventListener('pywebviewready', async function(e) {
 
 function handleUserInput(message) {
 
-    if (INPUT_TEXT.value == '') return;
+    INPUT_BTN.classList.add('send');
 
-        if (!CHATBOX.classList.contains('hasMessage')) {
-            CHATBOX.classList.add('hasMessage');
-            CHATBOX.textContent = '';
-        }
+    if (INPUT_TEXT.value == '') {
+        setTimeout(() => {
+            INPUT_BTN.classList.remove('send');
+        }, 100);
+        return;
+    }
 
-    
+    if (!CHATBOX.classList.contains('hasMessage')) {
+        CHATBOX.classList.add('hasMessage');
+        CHATBOX.textContent = '';
+    }
+
     message_handler('user', message);
     resize_input();
+
+    setTimeout(() => {
+        INPUT_BTN.classList.remove('send');
+    }, 100);
 }
 
 function message_handler(sender, message)  {
@@ -32,11 +42,27 @@ function message_handler(sender, message)  {
     // handle different messages
     switch (sender) {
         case 'user':
-            user(msgDiv, message)
+            user(msgDiv, message);
+            break;
+        case 'bot':
+            bot(msgDiv, message);
             break;
         default: 
             pywebview.api.log("No valid sender", 30)
     }
+}
+
+function bot(msgDiv, message) {
+
+    pywebview.api.log("Submitted by bot: " + message, 10);
+
+    msgDiv.textContent = message;
+    msgDiv.classList.add('message', 'bot')
+    
+    CHATBOX.appendChild(msgDiv)
+    INPUT_TEXT.value = '';
+    
+    chatbox.scrollTop = chatbox.scrollHeight;
 }
 
 function user(msgDiv, message) {
@@ -44,8 +70,8 @@ function user(msgDiv, message) {
     pywebview.api.log("Submitted by user: " + message, 10);
 
     msgDiv.textContent = message;
-    msgDiv.classList.add('message', 'user')
-    
+    msgDiv.classList.add('message', 'user');
+
     CHATBOX.appendChild(msgDiv)
     INPUT_TEXT.value = '';
     
